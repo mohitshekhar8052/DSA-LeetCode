373aaf4e3ea17252d347dbc0bebadb9884c741a6
Mohit Shekhar <168738080+mohitshekhar8052@users.noreply.github.com>
2026-01-04 15:50:27 +0530

    Time: 6 ms (43.43%), Space: 49.1 MB (5.52%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,24 +1,67 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
 
-        int left = 0;
-        int right = n - 1;
-        int idx = n - 1;
+        List<Integer> neg = new ArrayList<>();
+        List<Integer> pos = new ArrayList<>();
 
-        while (left <= right) {
-            int leftSq = nums[left] * nums[left];
-            int rightSq = nums[right] * nums[right];
-
-            if (leftSq > rightSq) {
-                result[idx--] = leftSq;
-                left++;
+        // split negatives & positives
+        for (int num : nums) {
+            if (num < 0) {
+                neg.add(num);
             } else {
-                result[idx--] = rightSq;
-                right--;
+                pos.add(num);
             }
         }
-        return result;
+
+        // if only positives
+        if (neg.size() == 0) {
+            for (int i = 0; i < pos.size(); i++) {
+                pos.set(i, pos.get(i) * pos.get(i));
+            }
+            return pos.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        // if only negatives
+        if (pos.size() == 0) {
+            for (int i = 0; i < neg.size(); i++) {
+                neg.set(i, neg.get(i) * neg.get(i));
+            }
+            Collections.reverse(neg);
+            return neg.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        // square negatives
+        for (int i = 0; i < neg.size(); i++) {
+            neg.set(i, neg.get(i) * neg.get(i));
+        }
+        Collections.reverse(neg);
+
+        // square positives
+        for (int i = 0; i < pos.size(); i++) {
+            pos.set(i, pos.get(i) * pos.get(i));
+        }
+
+        // merge two sorted lists
+        int i = 0, j = 0, id = 0;
+        int n1 = neg.size(), n2 = pos.size();
+        int[] res = new int[n1 + n2];
+
+        while (i < n1 && j < n2) {
+            if (neg.get(i) <= pos.get(j)) {
+                res[id++] = neg.get(i++);
+            } else {
+                res[id++] = pos.get(j++);
+            }
+        }
+
+        while (i < n1) {
+            res[id++] = neg.get(i++);
+        }
+
+        while (j < n2) {
+            res[id++] = pos.get(j++);
+        }
+
+        return res;
     }
 }
